ca8554e9b56d5d3205f1546efd73ca5738920455
Kit <kit@rushdigital.co.nz>
2016-10-27 17:33:29 +1300

    no message

--- source/Assets/Experiments/HexMap1/Scripts/HexGrid.cs
+++ source/Assets/Experiments/HexMap1/Scripts/HexGrid.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class HexGrid : MonoBehaviour
 {
@@ -7,12 +8,16 @@ public class HexGrid : MonoBehaviour
     public int height = 6;
 
     public HexCell cellPrefab;
+    public Text cellLabelPrefab;
 
-    HexCell[] cells;
+    private Canvas gridCanvas;
+    private HexCell[] cells;
+
 
     void Awake()
     {
         cells = new HexCell[height * width];
+        gridCanvas = GetComponentInChildren<Canvas>();
 
         for (int z = 0, i = 0; z < height; z++)
         {
@@ -26,13 +31,18 @@ public class HexGrid : MonoBehaviour
     void CreateCell(int x, int z, int i)
     {
         Vector3 position;
-        position.x = x * 10f;
+        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
         position.y = 0f;
-        position.z = z * 10f;
+        position.z = z * (HexMetrics.outerRadius * 1.5f);
 
         HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
         cell.transform.SetParent(transform, false);
         cell.transform.localPosition = position;
-    }
 
+        Text label = Instantiate<Text>(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition =
+            new Vector2(position.x, position.z);
+        label.text = x.ToString() + "\n" + z.ToString();
+    }
 }
